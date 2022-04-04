e16072b1c58dec66c53eb9cc77d7c76703181e0f
Nathan Rowbottom <nathan.rowbottom@snpolytechnic.com>
2022-04-04 09:55:19 -0400

    Small fixes class

--- A_Ship.java
+++ A_Ship.java
@@ -1,12 +1,12 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class AShip here.
+ * Write a description of class A_Ship here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class AShip extends VecActor implements I_Damageable
+public class A_Ship extends VecActor implements I_Damageable
 {
     
 }
