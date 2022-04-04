a0dabffa613311e7eb966650dbfb5a7579d5b975
Cole Spiers <cs18ii@snpsteam.com>
2022-04-04 10:21:22 -0400

    Ship

--- A_Ship.java
+++ A_Ship.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class A_Ship extends VecActor implements I_Damageable
+public abstract class A_Ship extends VecActor implements I_Damageable
 {
     
 }
--- Ship.java
+++ Ship.java
@@ -8,6 +8,21 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Ship extends A_Ship
 {
+    //double speed = 0; 
+    //int maxSpeed = 10;
+    //double thrust = 0.5;
+    //double thrustBack = 0.25;
+    //int turnSpeed = 4;
+    //int shotcooldown = 15;
+    //int shotTimer = 0;
+    int Health;
+    
+    //World world;
+    //GreenfootImage img;
+    //MouseInfo mouse;
+    
+    //Healthbar healthbar;
+    //int maxhealth = 600;
     /**
      * 
      * Everything from I_Damageable that is needed here in ship This is just a big list of all the stuff from the implement
@@ -52,7 +67,7 @@ public class Ship extends A_Ship
     
     public int getHealth()
     {
-        
+        return Health;
     }
     
     
