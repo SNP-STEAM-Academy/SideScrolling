31f5872bb3923f3a43ef77cc53f293e64c588e42
Cole Spiers <cs18ii@snpsteam.com>
2022-04-04 10:16:01 -0400

    Ship

--- Ship.java
+++ Ship.java
@@ -8,5 +8,53 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Ship extends A_Ship
 {
-
+    /**
+     * 
+     * Everything from I_Damageable that is needed here in ship This is just a big list of all the stuff from the implement
+     * 
+     * from setHealthBar down to getHealth
+     * 
+     */
+    public void setHealthbar()//ensures the implementation should have a health bar
+    {
+        
+    }
+    
+    
+    //damage, heal, reset, setHealth, getHealth
+    public void damage(int amount)
+    {
+        
+    }
+    
+    
+    
+    public void heal(int amount)
+    {
+        
+    }
+    
+    
+    
+    public void reset()
+    {
+        
+    }
+    
+    
+    
+    public void setHealth(int amount)
+    {
+        
+    }
+    
+    
+    
+    public int getHealth()
+    {
+        
+    }
+    
+    
+    
 }
