8fd35f1e3741d3f8f43c27ac55b754b011ff4d6e
Cole Spiers <cs18ii@snpsteam.com>
2022-04-05 09:57:44 -0400

    added GifImage and did game, and added some of haidens ship images

--- Game.java
+++ Game.java
@@ -15,7 +15,77 @@ public class Game extends A_Game
      */
     public Game()
     {
+        ship = new Ship();
+        addObject(ship, getWidth()/2, getHeight()/2);
+        
+        // for(int i =0; i < Spawner.xLocations.length; i++)
+        // {
+            // Spawner temp = new Spawner();
+            // addObject(temp, Spawner.xLocations[i], Spawner.yLocations[i]);
+        // }
+        // addObject(new Enemy(), 200, 200);
+        // setPaintOrder(Ship.class, Bullet.class, Asteroid.class);
+        
+        // if(!DEBUG)
+        // {
+            // //calling a constructor for the title screen
+            // title = new TitleScreen();
+            // //setting the game for the active instince
+            // title.setGame(this);
+            // //then opening the title screen first before game
+            // Greenfoot.setWorld(title);
+        // }
+        
+        // if (!DEBUG && Greenfoot.isKeyDown("p"))
+        // {
+            // //calling for pause menu
+            // pause = new PauseMenu();
+            // pause.setGame(this);
+            // Greenfoot.setWorld(pause);
+        // }
     }
+    
+    public void act()
+    {
+        //display lives 
+        // if(lives>0)
+        // {
+            // showText("Lives: "+lives, 100, 100);
+        // }
+        // else
+        // {
+            // //else
+            // showText("Start Again?!?", 600, 350);
+            // showText("GAME OVER!!!", 600, 400);
+            // showText("",100,100);
+            // return;
+        // }
+        // //chek for ship in world
+        // int numShips =  getObjects(Ship.class).size() - getObjects(Enemy.class).size();
+        // if(numShips<=0)
+        // {
+            // //if no -1 life
+            // lives--;
+            // if(lives > 0)
+            // {
+                // ship = new Ship();
+                // addObject(ship, getWidth()/2, getHeight()/2);
+            // }           
+        // }
+        
+        // if (lives <= 2)
+        // { //power up
+            // if (Spawncooldown <= 0)
+            // {
+                // addObject(new powerUp(), 300, 300);
+                // //System.out.println("oof");
+            // }else Spawncooldown = Util.random(Minspawncooldown, Maxspawncooldown);
+            
+            // Spawncooldown--;
+        // }
+    }
+    
+    
     
     protected void updateBackground(){
         
