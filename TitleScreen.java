f8c71cd35be94229b6e1e22ebc2709e31768bcc9
Haiden Murphy <hm18gf@snpsteam.com>
2022-04-05 09:50:42 -0400

    Adding Title Screen

---
+++ TitleScreen.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TitleScreen here.
+ * 
+ * @author Haiden Murphy
+ * @version o4/04/2022
+ */
+public class TitleScreen extends A_TitleScreen
+{
+
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {
+    }
+}
