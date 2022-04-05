6637c46d246996aecaf36910050827013a85f551
unknown <luca.quacquarelli@mohakcollege.ca>
2022-04-05 10:04:30 -0700

    add Ast

--- A_Asteroid.java
+++ A_Asteroid.java
@@ -4,20 +4,30 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * Write a description of class A_Asteroids here.
  * Asteroids just move in one direction and spawn in smaller asteroids when shot
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Luca Quacquarelli) 
+ * @date 2022-04-05
  */
-public abstract class A_Asteroid extends VecActor
-{
+public abstract class A_Asteroid extends VecActor {
     
-   protected int size;
-   protected int minSize;
-   protected int maxSize;
+    // size  variables
+    protected int size;
+    protected int minSize;
+    protected int maxSize;
     
     // speed variables 
-   protected double speed; 
-   protected double minSpeed;
-   protected double maxSpeed;
+    protected double speed; 
+    protected double minSpeed;
+    protected double maxSpeed;
+    
+    protected int damage;
+    protected int Health;
+    protected int detected;
+    
+    protected String name;
+    
+    // Game and image variables 
+    //GreenfootImage img;
+    //Game game;
     
     
     /**
@@ -33,4 +43,18 @@ public abstract class A_Asteroid extends VecActor
      * Method split has not been created yet. 
      */
     public abstract void split();
-}
+    
+    
+    
+    
+    
+    /**
+     * Method getHealth
+     */
+    public abstract int getHealth();
+    
+    
+    
+    
+    
+}// end
