8cc3e51fdc243b6434b78bb759edfc80ec8076d1
Everett Luke Greene <eg18ys@snpsteam.com>
2022-04-14 11:11:30 -0400

    added stats and waves

---
+++ Stats.java
@@ -0,0 +1,61 @@
+/**
+ * Write a description of class Settings here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Stats  
+{
+    protected int maxHealth;
+    protected int overhealTimer;
+    //protected int burnTimer;
+    //protected int poisonTimer;
+    protected int decayTimer;
+    protected int invincibleTimer;
+    protected double Speed;
+    protected double turnSpeed;
+    protected double damageMult;
+    protected double damageConst;
+    protected double cooldownMult;
+    protected double colldownConst;
+    protected double amountBullets;
+    protected double shipType;
+    //protected double pilot;
+    protected double 
+    //protected double ;
+    
+    public Stats(int mH, int ohT, int dT, int bT, int pT, int iT, double mS, double mTS)
+    {
+        maxHealth = mH;
+        overhealTimer = ohT;
+        //burnTimer = bT;
+        //poisonTime = pT;
+        decayTimer = dT;
+        invincibleTimer = iT;
+        Speed = S;
+        turnSpeed = mTS;
+        
+        
+    }
+    
+    public int maxHealth()
+    {
+        return maxHealth;
+    }
+    
+    public int overhealTimer()
+    {
+        return overhealTimer;
+    }
+    
+    public void maxHealth(int mH)
+    {
+        //
+    }
+    /**
+    public void maxHealth(int mH)
+    {
+        maxHealth = mH;
+    }
+    */
+}
