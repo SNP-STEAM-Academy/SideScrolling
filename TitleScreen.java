1117d39385ea136410dbac9e587219c3fa7393d8
Haiden Murphy <hm18gf@snpsteam.com>
2022-04-16 15:22:22 -0400

    UUUUU

--- TitleScreen.java
+++ TitleScreen.java
@@ -10,19 +10,11 @@ public class TitleScreen extends A_TitleScreen
 {
     
     private GifImage gifImg = new GifImage("TitleScreenboth.gif");//sets new GifIMage
-<<<<<<< Updated upstream
-
-    //int width;//NR=>HM uset getWidth and getHeight instead                                                         //w
-    //int height;                                                        //h
+    //int width;//NR=>HM uset getWidth and getHeight instead              
+    //int height;
     //protected Game game; //NR=>HM moved to abstract class
     GreenfootSound myMusic = new GreenfootSound("Title.mp3");//NR=>HM just commenting this until you have music to use
-=======
-    //GifImage gifImg; 
-    int width;                                                         //w
-    int height;                                                        //h
-    private Game game;
-    GreenfootSound myMusic = new GreenfootSound("");
->>>>>>> Stashed changes
+    
     
     /**
      * Constructor for objects of class TitleScreen.
@@ -32,14 +24,12 @@ public class TitleScreen extends A_TitleScreen
     {
         super(1000, 600);
         //gifImg = templateGif; //gifImg
-<<<<<<< Updated upstream
         //This is the main screen we are starting in now.  How?
         game = g;
         
         //(gifImg);                 
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        GreenfootImage img = new GreenfootImage(500,500);//NR Is this for a button ?
-=======
+        GreenfootImage img = new GreenfootImage(500,500);//NR Is this for a button?
         
         //call the onstructer for title screen
         //Greenfoot.setWorld(title);
@@ -48,25 +38,19 @@ public class TitleScreen extends A_TitleScreen
         //(gifImg);                 
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         GreenfootImage img = new GreenfootImage(500,500);
->>>>>>> Stashed changes
         //(new Label("Click Anywhere to Contitue", 25), getWidth()/2, (int)(getHeight() * 0.7));
     }
     
     public void act()
     {
         setImage();
-<<<<<<< Updated upstream
         myMusic.play();//NR=>HM just commenting this until you have music to use
-=======
-        myMusic.play();
->>>>>>> Stashed changes
         if (Greenfoot.mouseClicked(this))
         {//
             Util.say(""+game);
             Greenfoot.setWorld(game);
         }//
     }
-<<<<<<< Updated upstream
     /* NR=> HM removed this as responisibility should lie with the constructor
     public void setGame(Game g)
     {
@@ -75,23 +59,17 @@ public class TitleScreen extends A_TitleScreen
 
     }
     */
-=======
     
     public void setGame(Game g)
     {
         this.game = g;
     }
     
->>>>>>> Stashed changes
     protected void setImage()
     {
         GreenfootImage temp = gifImg.getCurrentImage();
         //temp.scale(getWidth(), getHeight());//comment out if image size changed
         
-<<<<<<< Updated upstream
-=======
-        
->>>>>>> Stashed changes
         setBackground(temp);
     }
 }
